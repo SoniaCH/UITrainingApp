bca50f124068397ad8fc0ddd14b554ff844bbb5a
SoniaChnini <chnini.sonia@gmail.com>
2018-03-13 11:03:19 +0100

    Add project files.

---
+++ UITrainingApp/UITrainingApp/Model/Pages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UITrainingApp.Model
+{
+    public class Pages
+    { public string Title { get; set; }
+      public string Number { get; set; }
+             
+    }
+}
---
+++ UITrainingApp/UITrainingApp/Views/HomePage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using UITrainingApp.Model;
+
+namespace UITrainingApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : MasterDetailPage
+	{
+        private List<Pages> pageList;
+        public List<Pages> PageList
+        {
+            get { return pageList; }
+            set
+            {
+                pageList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        List<Pages> _menu = new List<Pages>();
+        public HomePage ()
+		{
+            Detail = new NavigationPage(new DetailPage());
+            InitializeComponent();
+
+            //ListView1.ItemsSource = new string[]
+            //{
+            //    "My account",
+            //    "Shop",
+            //    "FAQ",
+            //    "Notifications"
+
+            //};
+
+            IsPresented = true;
+
+            PageList = LoadTheListOfPages();
+            ListView1.ItemsSource = PageList;
+            
+
+
+        }
+
+        #region Class to load the menu list
+        public List<Pages> LoadTheListOfPages()
+        {
+            _menu.Add(new Pages() { Title = "MY ACCOUNT", Number = "" });
+            _menu.Add(new Pages() { Title = "SHOP", Number = "" });
+            _menu.Add(new Pages() { Title = "FAQ", Number = "" });
+            _menu.Add(new Pages() { Title = "NOTIFICATIONS", Number = "2" });
+            return _menu;
+        }
+        #endregion
+
+    }
+}
